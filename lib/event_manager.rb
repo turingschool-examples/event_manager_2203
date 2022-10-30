144d41de3445c9cb5875bd619baf2011403b0049
Ryan Canton <ryancanton999@gmail.com>
2022-10-30 10:28:30 -0600

    Initial commit; I was so carried away following the lesson I did not treat this program like a traditional assignment and finished through iteration 2

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,18 @@
 # ./lib/event_manager.rb
-require "csv"
+require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
 puts "EventManager initialized."
+
+contents = CSV.open './data/event_attendees.csv', headers: true, header_converters: :symbol
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
+end
